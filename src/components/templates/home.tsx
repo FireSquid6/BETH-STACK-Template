0e9704ff1925cc14b57a2625c5607ca90c3a9d9f
firesquid <jdeiss06@gmail.com>
2023-12-04 17:42:37 -0600

    add some tailwind to remove warning

--- src/components/templates/home.tsx
+++ src/components/templates/home.tsx
@@ -3,7 +3,7 @@ import { BaseHTML } from "./base";
 export const Home = () => {
   return (
     <BaseHTML>
-      <main>
+      <main class="flex flex-row h-full w-full">
         <p>Hello world!</p>
       </main>
     </BaseHTML>
